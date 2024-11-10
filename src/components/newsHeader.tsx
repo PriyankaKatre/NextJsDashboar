23f3763e90ddd40a628f8392b9f018c9404b4804
Priyanka Katre <priyankack1707@gmail.com>
2024-11-10 02:04:02 +0000

    implement dark/light mode switch

--- src/components/newsHeader.tsx
+++ src/components/newsHeader.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTh } from "@fortawesome/free-solid-svg-icons";
+import { Switch } from "@/components/ui/switch";
 
 interface NewsHeaderProps {
   setIsListView: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const NewsHeader: React.FC<NewsHeaderProps> = ({ setIsListView }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  console.log("document.body", document.body);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkMode]);
   return (
     <nav className="flex justify-between items-center">
       <div className="text-2xl font-bold">
@@ -20,6 +30,7 @@ const NewsHeader: React.FC<NewsHeaderProps> = ({ setIsListView }) => {
           <FontAwesomeIcon icon={faBars} className="h-4 w-4 text-gray-800" />
           <span className="text-gray-500 ml-2">List View</span>
         </div>
+        <Switch checked={isDarkMode} onCheckedChange={setIsDarkMode} />
         <div
           className="ml-5 cursor-pointer"
           onClick={() => setIsListView(false)}
