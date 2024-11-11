49e19fa96cba64c3478a50e77371590c9b318164
Priyanka Katre <priyankack1707@gmail.com>
2024-11-11 14:48:37 +0000

    update bar and line graph

--- src/components/graph/bar.tsx
+++ src/components/graph/bar.tsx
@@ -2,56 +2,58 @@ import React, { useRef, useEffect } from "react";
 import Chart from "chart.js/auto";
 
 const Bar = () => {
-  const chartRef = useRef(null);
-  const chartInstanceRef = useRef(null);
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const chartInstanceRef = useRef<Chart | null>(null);
 
   useEffect(() => {
-    const ctx = chartRef?.current?.getContext("2d");
+    const ctx = chartRef.current?.getContext("2d");
 
     if (chartInstanceRef.current) {
-      chartInstanceRef?.current?.destroy();
+      chartInstanceRef.current.destroy();
     }
 
-    chartInstanceRef?.current = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-        ],
-        datasets: [
-          {
-            label: "Sales Performance",
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: "rgb(195,167,255)",
-            borderColor: "rgb(195,167,255)",
-            borderWidth: 1,
+    if (ctx) {
+      chartInstanceRef.current = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+          ],
+          datasets: [
+            {
+              label: "Sales Performance",
+              data: [65, 57, 80, 80, 58, 58, 40],
+              backgroundColor: "rgb(195,167,255)",
+              borderColor: "rgb(195,167,255)",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top",
+            },
+            title: {
+              display: true,
+              text: "Sales Performance",
+            },
           },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "Sales Performance",
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
           },
         },
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
+      });
+    }
 
     return () => {
       if (chartInstanceRef.current) {
--- src/components/graph/line.tsx
+++ src/components/graph/line.tsx
@@ -2,56 +2,52 @@ import React, { useRef, useEffect } from "react";
 import Chart from "chart.js/auto";
 
 const Line = () => {
-  const chartRef = useRef();
-  const chartInstanceRef = useRef();
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const chartInstanceRef = useRef<Chart | null>(null);
 
   useEffect(() => {
-    const ctx = chartRef?.current?.getContext("2d");
+    const ctx = chartRef.current?.getContext("2d");
 
-    // Destroy the existing chart instance before creating a new one
     if (chartInstanceRef.current) {
-      chartInstanceRef?.current?.destroy();
+      chartInstanceRef.current.destroy();
     }
 
-    chartInstanceRef?.current = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6", "Day7"],
-        datasets: [
-          {
-            label: "Active Users",
-            data: [12, 19, 3, 5, 2, 3, 7],
-            fill: false,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "User Activity",
-          },
+    if (ctx) {
+      chartInstanceRef.current = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6", "Day7"],
+          datasets: [
+            {
+              label: "Active Users",
+              data: [12, 19, 3, 5, 2, 3, 7],
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderWidth: 1,
+            },
+          ],
         },
-        scales: {
-          y: {
-            min: 2,
-            max: 20,
-            ticks: {
-              stepSize: 2,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top",
+            },
+            title: {
+              display: true,
+              text: "User Activity",
+            },
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
             },
           },
         },
-      },
-    });
+      });
+    }
 
     return () => {
-      // Cleanup function to destroy the chart instance when the component unmounts
       if (chartInstanceRef.current) {
         chartInstanceRef.current.destroy();
       }
