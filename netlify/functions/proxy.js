5cedd5ace76e48c11a308065c330c4a4073484ef
Priyanka Katre <priyankack1707@gmail.com>
2024-11-11 17:00:10 +0000

    fix issue

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,8 +1,14 @@
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
-  const { url } = event.queryStringParameters;
+  const {
+    city,
+    country = "us",
+    category = "business",
+  } = event.queryStringParameters;
   const apiKey = process.env.NEXT_PUBLIC_NEWS_SECRET_KEY;
+
+  const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`;
 
   try {
     const response = await axios.get(url);
