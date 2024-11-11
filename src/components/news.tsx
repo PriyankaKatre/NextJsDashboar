1668a597b8682b21306ebfc201dbc1a46c326d7b
Priyanka Katre <priyankack1707@gmail.com>
2024-11-11 12:15:27 +0000

    fix import issues

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -4,7 +4,7 @@ import { RootState, AppDispatch } from "@/redux/store";
 import { setLoading, setArticles, setError } from "@/redux/newsSlice";
 import { useFetchData } from "@/hooks/useFetchData";
 import NewsCard from "@/components/newsCard";
-import NewsHeader from "@/components/";
+import NewsHeader from "@/components/newsHeader";
 import { Loader2 } from "lucide-react";
 
 const NewsList: React.FC = ({ isListView }: boolean) => {
